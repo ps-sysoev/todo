695bfa3faec5eb0d485f5f6d73943ad496c198b3
Pavel Sysoev <pashgan22@yandex.ru>
2022-06-01 23:10:24 +0700

    Added filter function.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,7 +14,8 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
     ],
-    term: ''
+    term: '',
+    filter: ''
   };
 
   createTodoItem(label) {
@@ -97,9 +98,27 @@ export default class App extends Component {
     });
   };
 
+  filter(items, filter) {
+    switch (filter) {
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter((item) => !item.done);
+      case 'done':
+        return items.filter((item) => item.done);
+      default:
+        return items;
+    }
+  };
+
+  onFilterChange(filter) {
+    this.setState({filter: filter});
+    console.log(filter)
+  };
+
   render() {
-    const {todoData, term} = this.state;
-    const visibleItems = this.search(todoData, term);
+    const {todoData, term, filter} = this.state;
+    const visibleItems = this.filter(this.search(todoData, term), filter);
     const doneCount = todoData
       .filter((item) => item.done)
       .length;
@@ -111,7 +130,9 @@ export default class App extends Component {
         <div className='top-panel d-flex'>
           <SearchPanel
             onSearchChange={this.onSearchChange} />
-          <ItemStatusFilter />
+          <ItemStatusFilter
+            filter={filter}
+            onFilterChange={this.onFilterChange} />
         </div>
 
         <TodoList
